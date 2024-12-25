f177847e71e8f5dc671e5aee2312a456bab86ddf
KDhanak <kishandhanak306@gmail.com>
2024-12-25 18:14:17 +1100

    updates in the model.

--- backend/financestralia/models.py
+++ backend/financestralia/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Advisor(models.Model):
     first_name = models.CharField(max_length=500)
     last_name = models.CharField(max_length=500)
-    email = models.EmailField(max_length=500)
+    email = models.EmailField(unique=True)
     office = models.CharField(max_length=500)
     speciality = models.CharField(max_length=500)
     gender = models.CharField(max_length=500)
