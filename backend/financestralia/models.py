a3006936045ea48ecff593801ff870801cfa3043
KDhanak <kishandhanak306@gmail.com>
2024-12-26 11:21:59 +1100

    updates in the model.

--- backend/financestralia/models.py
+++ backend/financestralia/models.py
@@ -4,7 +4,6 @@ class Advisor(models.Model):
     first_name = models.CharField(max_length=500)
     last_name = models.CharField(max_length=500)
     email = models.EmailField(max_length=500)
-    office = models.CharField(max_length=500, null=True, blank=True)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
