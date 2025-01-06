eef8750dae090fcdfd34854169e578ea9ed91361
KDhanak <kishandhanak306@gmail.com>
2025-01-06 19:30:33 +1100

    args and kwargs.

--- backend/financestralia/views.py
+++ backend/financestralia/views.py
@@ -5,7 +5,7 @@ from .models import Advisor
 from .serializers import AdvisorSerializer
 
 class AdvisorView(APIView):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         items = Advisor.objects.all()
         serializer = AdvisorSerializer(items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
