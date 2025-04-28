b9bd7026c799023bda8b6c8967d80a894aa34e2c
KDhanak <kishandhanak306@gmail.com>
2025-04-28 20:19:04 +1000

    Mobile Responsive - Advisors

--- frontend/src/components/Advisors/Advisors.jsx
+++ frontend/src/components/Advisors/Advisors.jsx
@@ -84,7 +84,7 @@ const Advisors = () => {
                     <span className="absolute -bottom-1 left-0 w-1/3 h-[2px] bg-primary_5"></span>
                 </p>
                 <div>
-                    <form className="flex flex-row justify-center border border-white items-center space-x-5 py-3" onSubmit={(e) => filterData(e)}>
+                    <form className="hidden tablet:flex flex-row justify-center border border-white items-center space-x-5 py-3" onSubmit={(e) => filterData(e)}>
                         <input className="bg-primary_4 h-10 border-2 border-primary_4 focus:border-primary_5 placeholder:text-primary_3 tablet:w-40 laptop:w-56 text-center text-primary_3 outline-none" placeholder="Advisor Name" value={name} onChange={(e) => setName(e.target.value)}></input>
                         <p className="text-primary_1 font-medium my-3">OR</p>
                         <input className="bg-white h-10 border-2 border-primary_4 focus:border-primary_5 tablet:w-40 laptop:w-56 text-center text-primary_1 outline-none" placeholder="Postcode" value={postcode} onChange={(e) => setPostcode(e.target.value)}></input>
@@ -116,11 +116,44 @@ const Advisors = () => {
                         </div>
                         <button className="border-2 border-primary_1 py-2 px-5 text-primary_3 font-medium bg-primary_2 active:bg-primary_4 active:text-primary_5 active:border-primary_5 ease-in-out duration-150">Find</button>
                     </form>
+
+                    <form className="flex tablet:hidden flex-col justify-center border border-white items-center" onSubmit={(e) => filterData(e)}>
+                        <input className="bg-primary_4 h-10 border-2 border-primary_4 focus:border-primary_5 placeholder:text-primary_3 tablet:w-40 laptop:w-56 text-center text-primary_3 outline-none" placeholder="Advisor Name" value={name} onChange={(e) => setName(e.target.value)}></input>
+                        <p className="text-primary_1 font-medium my-1">OR</p>
+                        <input className="bg-white h-10 border-2 border-primary_4 focus:border-primary_5 tablet:w-40 laptop:w-56 text-center text-primary_1 outline-none" placeholder="Postcode" value={postcode} onChange={(e) => setPostcode(e.target.value)}></input>
+                        <p className="text-primary_1 font-medium my-1">OR</p>
+                        <div className="relative" ref={dropdownRef}>
+                            <div
+                                className="flex items-center justify-between bg-white h-auto border-2 border-primary_4 tablet:w-40 laptop:w-56 text-center text-primary_1 outline-none py-2 px-3 cursor-pointer"
+                                onClick={() => setShowList(!showList)}
+                            >
+                                <span>{selected}</span>
+                                {showList ? <FaArrowUp /> : <FaArrowDown />}
+                            </div>
+                            {showList && (
+                                <ul
+                                    className="absolute bg-white border border-primary_4 w-56 mt-1 z-10 overflow-y-auto max-h-96"
+                                    onClick={handleSpecialityDropDown}
+                                >
+                                    {speciality.map((item, index) => (
+                                        <li
+                                            key={index}
+                                            className="py-2 px-4 hover:bg-primary_5 hover:text-white cursor-pointer"
+                                            onClick={() => { setShowList(false); setSelected(item) }}
+                                        >
+                                            {item}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                        <button className="mt-3 border-2 border-primary_1 py-1 px-3 text-primary_3 font-medium bg-primary_2 active:bg-primary_4 active:text-primary_5 active:border-primary_5 ease-in-out duration-150">Find</button>
+                    </form>
                 </div>
-                <p onClick={handleReset} className="self-center text-lg hover:underline text-primary_5 -mt-2 mb-2 cursor-pointer">Reset Filters</p>
+                <p onClick={handleReset} className="my-2 self-center text-lg hover:underline text-primary_5 cursor-pointer">Reset Filters</p>
             </div >
             <div className="flex flex-col">
-                <div className="tablet:mx-[50px] laptop:mx-[100px] lLaptop:mx-[300px] monitor:mx-[500px] 4K:mx-[750px] flex flex-col space-y-5">
+                <div className="hidden tablet:flex flex-col tablet:mx-[50px] laptop:mx-[100px] lLaptop:mx-[300px] monitor:mx-[500px] 4K:mx-[750px] space-y-5">
                     {filteredData.map((dataItem, index) => (
                         <div key={index} className="flex bg-primary_3">
                             <div className="w-1/5">
@@ -144,6 +177,35 @@ const Advisors = () => {
                                     <p className="text-primary_1 font-medium">Office</p>
                                     <p className="text-primary_1 font-normal">{dataItem.office}</p>
                                 </div>
+                            </div>
+                        </div>
+
+                    ))}
+                </div>
+
+                <div className="tablet:hidden flex flex-col space-y-5">
+                    {filteredData.map((dataItem, index) => (
+                        <div key={index} className="flex flex-col bg-primary_3">
+                            <div className="flex">
+                                <div className="w-4/12 h-1/2">
+                                    <img className="size-24 p-2" src={(dataItem.gender === "M") ? profileImage1 : profileImage2} />
+                                    <p className="text-primary_1 px-2 py-1 font-medium text-sm">{dataItem.first_name} {dataItem.last_name}</p>
+                                    <p className="text-primary_4 px-2 -mt-1 font-normal text-xs">{dataItem.role}</p>
+                                </div>
+                                <div className="w-5/12 mt-5">
+                                    <p className="text-primary_1 font-medium text-sm">My Speciality</p>
+                                    <p className="text-primary_5 text-xs mb-5 font-normal items-start relative">{dataItem.speciality}
+                                        <span className="absolute -bottom-1 left-0 w-14 h-[1px] bg-primary_2"></span>
+                                    </p>
+                                </div>
+                                <div className="mt-5 w-3/12">
+                                    <p className="text-primary_1 font-medium text-sm">Office</p>
+                                    <p className="text-primary_1 font-normal text-xs">{dataItem.office}</p>
+                                </div>
+                            </div>
+                            <div className="mb-1 p-2">
+                                <p className="text-primary_1 font-medium text-sm">Email</p>
+                                <p className="text-primary_1 font-normal text-xs -mt-1">{dataItem.email}</p>
                             </div>
                         </div>
 
