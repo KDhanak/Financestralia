af73a56a8e8154a8df049146de4c4e53be8b049d
KDhanak <kishandhanak306@gmail.com>
2025-04-29 01:08:56 +1000

    registering table query to admin.

--- backend/financestralia/admin.py
+++ backend/financestralia/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Advisor
+from .models import Advisor, Query
 
 # Register your models here.
 
 admin.site.register(Advisor)
+admin.site.register(Query)
